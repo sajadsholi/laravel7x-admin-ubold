ee9589604e6ee4e4d4f57e1fc1ac7c33798a2061
Sajad Sholi <sholi.sajad16@gmail.com>
2020-12-31 18:52:51 +0330

    fix config and add timezone

--- public/asset/admin/js/common.js
+++ public/asset/admin/js/common.js
@@ -36,7 +36,7 @@ function clearLocal() {
 }
 
 const capitalize = (str, lower = false) =>
-    (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());;
+    (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
 
 // =======local storage & session storage=======//
 
@@ -64,6 +64,22 @@ function myTooltip() {
 }
 // tooltip
 
+// convert number to price format with numeral js
+function convertPrice(elem) {
+    var value = numeral(elem.val()).value();
+    if (value == null) {
+        value = 0;
+    }
+    elem.val(numeral(value).format('0,0'));
+    if (elem.siblings('.basePrice').length != 0) {
+        elem.siblings('.basePrice').val(value);
+    }
+    if (elem.parents('.col-auto').find('.basePrice').length != 0) {
+        elem.parents('.col-auto').find('.basePrice').val(value);
+    }
+}
+// convert number to price format with numeral js
+
 baseRoot = $('#baseRoot').html().trim();
 baseAdminUrl = baseRoot + '/admin';
 assetPath = baseRoot + '/asset/admin'
@@ -76,6 +92,7 @@ $("img").on('error', function() {
 $(function() {
 
     var csrf = $('input[name=_token]').val();
+    var timer;
 
     $.ajaxSetup({
 
@@ -132,12 +149,28 @@ $(function() {
         dateFormat: "Y-m-d H:i:S",
         time_24hr: true
     });
+    $(".datePicker").flatpickr({
+        "locale": locale,
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        time_24hr: false
+    });
 
     // ==============flatpicker config==================
 
     //***********************************************
     //***********************************************
     //***********************************************
+
+
+    // ============numeral js config====================
+
+
+    $('.convertPrice').on('keyup', function() {
+        convertPrice($(this));
+    });
+
+    // ============numeral js config====================
 
     // ==============toaster config=====================
     if ($('html').attr('dir') == 'rtl') {
@@ -171,11 +204,12 @@ $(function() {
     if ($('.ck-editor').length > 0) {
 
         $('.ck-editor').each(function() {
+            var accept = $(this).data('accept');
             CKEDITOR.replace($(this).attr('id'), {
                 language: $('html').attr('lang'),
                 skin: 'icy_orange',
-                filebrowserBrowseUrl: "/fileman/index.html",
-                filebrowserImageBrowseUrl: '/fileman/index.html?type=image&langCode=' + $('html').attr('lang'),
+                filebrowserBrowseUrl: mediaRoute('atriatech.media.index'),
+                filebrowserImageBrowseUrl: mediaRoute('atriatech.media.index') + `?accept=${accept}`,
                 removeDialogTabs: 'link:upload;image:upload'
             });
         });
@@ -184,10 +218,11 @@ $(function() {
 
     /* ckeditor plugin simple */
     if ($('.ck-editor-simple').length > 0) {
+        var accept = $(this).data('accept');
         CKEDITOR.replace($('.ck-editor-simple').attr('id'), {
             language: $('html').attr('lang'),
-            filebrowserBrowseUrl: "/fileman/index.html",
-            filebrowserImageBrowseUrl: '/fileman/index.html?type=image&langCode=' + $('html').attr('lang'),
+            filebrowserBrowseUrl: mediaRoute('atriatech.media.index'),
+            filebrowserImageBrowseUrl: mediaRoute('atriatech.media.index') + `?accept=${accept}`,
             removeDialogTabs: 'link:upload;image:upload',
             toolbarGroups: [
                 { name: 'tools', groups: ['tools'] },
@@ -293,11 +328,16 @@ $(function() {
                     success: function() {
                         // increase the trash count
                         var trashCount = $('#trashCount').html();
-                        if (!trashCount != undefined) {
+                        if (trashCount != undefined) {
                             $('#trashCount').html(Number(trashCount) + 1);
                         }
 
-                        Swal.fire("Done", window.i18n.common['successMsg'], "success");
+                        Swal.fire({
+                            title: "Done",
+                            html: window.i18n.common['successMsg'],
+                            type: "success",
+                            onClose: deleteModalOnClose
+                        });
 
                         var tbody = elem.parents('tbody')
                         var checkTr = elem.parents('tr');
@@ -323,7 +363,83 @@ $(function() {
             }
         })
     }
+
+    function deleteModalOnClose() {
+
+        if ($('.trashButton').length > 0) {
+
+            $("html, body").animate({
+                scrollTop: $(".trashButton").offset().top - 130
+            }, "800", "swing");
+
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                $(".trashButton").focus();
+            }, 1000);
+            timer = setTimeout(() => {
+                $(".trashButton").blur();
+            }, 2500);
+
+        }
+
+    }
     // delete item
+
+    // restore item
+    $(".restoreItem").on('click', function() {
+        restoreItem($(this));
+    });
+
+    function restoreItem(elem) {
+
+        var name = elem.data('name');
+        var url = elem.data('url');
+        if ($('html').attr('dir') == 'ltr') {
+            var reverseButtonsDir = !0;
+        } else {
+            var reverseButtonsDir = 0;
+        }
+
+        Swal.fire({
+            title: window.i18n.common['askTitle'],
+            html: window.i18n.common['restore'] + ' : ' + name,
+            type: "warning",
+            showCancelButton: !0,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: window.i18n.common['restoreBtn'],
+            cancelButtonText: window.i18n.common['cancel'],
+            reverseButtons: reverseButtonsDir
+        }).then(function(t) {
+            if (t.value == true) {
+                $.ajax({
+                    url: url,
+                    method: 'POST',
+                    success: function() {
+
+                        Swal.fire("Done", window.i18n.common['successMsg'], "success");
+
+                        elem.closest('tr').remove();
+                        var check = $('tbody tr');
+                        var countTh = $('thead tr th').length;
+                        var checkAdditionalTr = $('tbody').find('.additionalTr');
+                        if ((check.length - checkAdditionalTr.length) <= 0) {
+                            $('tbody').html(`<tr><td colspan="${countTh}" class="alert-danger p-2 text-center">${ window.i18n.common['noResult'] }</td></tr>`);
+                        }
+
+                    },
+                    error: function() {
+                        Swal.fire("Error!", window.i18n.common['errorMsg'], "error");
+                    }
+                });
+
+            }
+        })
+
+
+
+    }
+    // restore item
 
 
     // activate and inactive status
@@ -436,68 +552,78 @@ $(function() {
     // seconds
 
     // =============== get the active sidebar ============//
-    var URL = window.location.href.split(/[?#]/)[0];
-    var mostSimilarLinkScore = 0;
-    var mostSimilarLink;
-    $.each($('.slimscroll-menu a'), function(indexInArray, valueOfElement) {
-        checkSimilarity(URL, $(this).attr('href'));
-    });
+    if ($("#side-menu a.active").length > 0) {
 
-    function checkSimilarity(currentUrl, link) {
-        var currentSimilarity = similarity(currentUrl, link);
-        if (currentSimilarity > mostSimilarLinkScore) {
-            mostSimilarLinkScore = currentSimilarity;
-            mostSimilarLink = link;
-        }
-    }
-    if (mostSimilarLinkScore > 0 && mostSimilarLinkScore < 1) {
-        var targetElement = $(`.slimscroll-menu .metismenu > li:not(.adminLi)`).find(`a[href^="${mostSimilarLink}"]`);
-        if (targetElement.length > 0) {
-            targetElement.parents('li').parents('li').addClass('active');
-            targetElement.parents('li').parents('li').find('a').addClass('active');
-            targetElement.parents('.nav-second-level').addClass('in');
-        }
-    }
+        targetElement = $("#side-menu a.active");
+        targetElement.parents('li').addClass('active');
+        targetElement.parents('.nav-second-level').addClass('in');
+        targetElement.parents('.nav-third-level').addClass('in');
 
-    function similarity(s1, s2) {
-        var longer = s1;
-        var shorter = s2;
-        if (s1.length < s2.length) {
-            longer = s2;
-            shorter = s1;
-        }
-        var longerLength = longer.length;
-        if (longerLength == 0) {
-            return 1.0;
-        }
-        return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
-    }
+    } else {
+        var URL = window.location.href.split(/[?#]/)[0];
+        var mostSimilarLinkScore = 0;
+        var mostSimilarLink;
+        $.each($('.slimscroll-menu a'), function(indexInArray, valueOfElement) {
+            checkSimilarity(URL, $(this).attr('href'));
+        });
 
-    function editDistance(s1, s2) {
-        s1 = s1.toLowerCase();
-        s2 = s2.toLowerCase();
+        function checkSimilarity(currentUrl, link) {
+            var currentSimilarity = similarity(currentUrl, link);
+            if (currentSimilarity > mostSimilarLinkScore) {
+                mostSimilarLinkScore = currentSimilarity;
+                mostSimilarLink = link;
+            }
+        }
+        if (mostSimilarLinkScore > 0 && mostSimilarLinkScore < 1) {
+            var targetElement = $(`.slimscroll-menu .metismenu > li:not(.adminLi)`).find(`a[href^="${mostSimilarLink}"]`);
+            if (targetElement.length > 0) {
+                targetElement.parents('li').parents('li').addClass('active');
+                targetElement.parents('li').addClass('active');
+                targetElement.parents('li').parents('li').find('a').addClass('active');
+                targetElement.parents('.nav-second-level').addClass('in');
+            }
+        }
 
-        var costs = new Array();
-        for (var i = 0; i <= s1.length; i++) {
-            var lastValue = i;
-            for (var j = 0; j <= s2.length; j++) {
-                if (i == 0)
-                    costs[j] = j;
-                else {
-                    if (j > 0) {
-                        var newValue = costs[j - 1];
-                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                            newValue = Math.min(Math.min(newValue, lastValue),
-                                costs[j]) + 1;
-                        costs[j - 1] = lastValue;
-                        lastValue = newValue;
+        function similarity(s1, s2) {
+            var longer = s1;
+            var shorter = s2;
+            if (s1.length < s2.length) {
+                longer = s2;
+                shorter = s1;
+            }
+            var longerLength = longer.length;
+            if (longerLength == 0) {
+                return 1.0;
+            }
+            return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+        }
+
+        function editDistance(s1, s2) {
+            s1 = s1.toLowerCase();
+            s2 = s2.toLowerCase();
+
+            var costs = new Array();
+            for (var i = 0; i <= s1.length; i++) {
+                var lastValue = i;
+                for (var j = 0; j <= s2.length; j++) {
+                    if (i == 0)
+                        costs[j] = j;
+                    else {
+                        if (j > 0) {
+                            var newValue = costs[j - 1];
+                            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                                newValue = Math.min(Math.min(newValue, lastValue),
+                                    costs[j]) + 1;
+                            costs[j - 1] = lastValue;
+                            lastValue = newValue;
+                        }
                     }
                 }
+                if (i > 0)
+                    costs[s2.length] = lastValue;
             }
-            if (i > 0)
-                costs[s2.length] = lastValue;
+            return costs[s2.length];
         }
-        return costs[s2.length];
     }
 
     // =============== get the active sidebar ============//
@@ -511,7 +637,7 @@ $(function() {
         filters.push(hash[0]);
         filters[hash[0]] = hash[1];
     }
-    if (filters[0] == 'page' || window.location.href.indexOf('?') > 0) {
+    if ((filters[0] == 'page' || window.location.href.indexOf('?') > 0) && $(".table").length > 0) {
         $("html, body").animate({
             scrollTop: $(".table").parents('.row').offset().top - 130
         }, "800", "swing");
@@ -530,7 +656,6 @@ $(function() {
 
     if ($('.searchUser').length > 0) {
 
-        var url = $('.searchUser').data('url');
 
         $(".searchUser").select2({
             placeholder: window.i18n.common['searchUser'],
@@ -543,12 +668,13 @@ $(function() {
                 }
             },
             ajax: {
-                url: url,
+                url: $('.searchUser').data('url'),
                 method: 'POST',
                 delay: 250,
                 data: function(params) {
                     return {
-                        search: params.term
+                        search: params.term,
+                        type: $('.searchUser').data('type')
                     };
                 },
                 processResults: function(data) {
@@ -592,7 +718,6 @@ $(function() {
 
     // convert link
 
-    var timer;
 
     function hyphen(param, convert) {
         param = param.replace(/([~!@#$%^&*()_+=`{}\[\]\|\\:;'<>,.\/? ])+/g, '-').replace(/^(-)+|(-)+$/g, '-');
@@ -727,6 +852,65 @@ $(function() {
 
     // check email is unique
 
+    // check username is unique
+
+    $('.checkUsernameIsUnique').on("input", function() {
+
+        if ($(this).val().length <= 0) {
+            return;
+        }
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+
+            params = {
+                username: $(this).val(),
+                can: $(this).data("can"),
+                model: $(this).data("model"),
+                action: $(this).data("action"),
+                id: $(this).data('id'),
+            }
+
+            $.ajax({
+                type: "POST",
+                url: baseAdminUrl + "/common/checkUsernameIsUnique",
+                data: params,
+                success: function(response) {},
+                error: function(xhr, response) {
+                    if (xhr.status == 422) {
+                        toastr['warning'](window.i18n.validation.unique.replace(/:attribute/g, window.i18n.common.username));
+                    }
+                }
+            });
+
+        }, 2000);
+
+    });
+
+    // check username is unique
+
+    // meta-keywords-select
+    $.each($('.meta-keywords-select'), function(indexInArray, valueOfElement) {
+
+        var placeholder = $(this).data('placeholder');
+
+        $(this).select2({
+            placeholder: placeholder,
+            tags: true,
+        });
+
+    });
+
+    if ($('.meta-keywords-select').attr('title') != undefined) {
+        $(".select2-container").tooltip({
+            title: function() {
+                return $(this).prev().attr("title");
+            },
+            placement: "auto",
+            trigger: "focus"
+        });
+    }
+    // meta-keywords-select
+
     // keywords-select
     $.each($('.keywords-select'), function(indexInArray, valueOfElement) {
 
@@ -735,6 +919,25 @@ $(function() {
         $(this).select2({
             placeholder: placeholder,
             tags: true,
+
+            language: {
+                searching: function() {
+                    return window.i18n.common['searching'];
+                }
+            },
+
+            ajax: {
+                url: baseAdminUrl + '/search/keyword',
+                delay: 300,
+                method: 'POST',
+                processResults: function(data, page) {
+                    return {
+                        results: data
+                    };
+                }
+            }
+
+
         });
 
     });
@@ -748,6 +951,7 @@ $(function() {
             trigger: "focus"
         });
     }
+    // keywords-select
 
     $('.selectLanguage').on('click', function() {
         $('#languageSelect .dropdown-menu a.active').removeClass('active');
@@ -784,5 +988,84 @@ $(function() {
             }
         });
     }
+
+    if ($('#map').length > 0) {
+        loadMap();
+    }
+
+    function loadMap() {
+        var latInput = $('input[name=lat]');
+        var lngInput = $('input[name=lng]');
+
+        const lat = parseFloat($('#map').data('lat'));
+        const lng = parseFloat($('#map').data('lng'));
+
+        const map = L.map('map', {
+            scrollWheelZoom: true
+        }).setView([lat, lng], parseInt($('#map').data('zoom'), 10));
+
+        L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+            attribution: `<a href="${$('#source_info').attr('href')}" target="_blank">${$('#source_info').html()}</a>`
+        }).addTo(map);
+
+        map.on('movestart', function(e) {
+            $('.map-marker').addClass('active-marker');
+        }).on('moveend', function() {
+            $('.map-marker').removeClass('active-marker');
+
+            latInput.val(map.getCenter().lat);
+            lngInput.val(map.getCenter().lng);
+        });
+    }
+
+    if ($('#showLocation').length > 0) {
+        let location = {
+            lat: $('#showLocation').data('lat'),
+            lng: $('#showLocation').data('lng')
+        };
+        let map;
+        map = L.map('showLocation', {
+            center: [location.lat, location.lng],
+            zoom: 16,
+            drawControl: false
+        });
+        L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+            attribution: `<a href="${$('#source_info').attr('href')}" target="_blank">${$('#source_info').html()}</a>`
+        }).addTo(map);
+        let marker = L.marker([location.lat, location.lng], {
+            draggable: false
+        });
+        map.addLayer(marker);
+    }
+
+
+
+    // in view port event
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+
+    // set the user timezone
+    timezone = moment.tz.guess();
+    $.ajax({
+        type: "POST",
+        url: baseAdminUrl + "/config/timezone",
+        data: {
+            timezone: timezone
+        },
+        success: function(response) {
+            document.cookie = `timezone=${timezone}; expires=Thu, 18 Dec 2113 12:00:00 UTC; path=/`;
+        }
+    });
+    // set the user timezone
 
 });
